157b4e3d36d75e1f85b9918843bb4967ade60ad2
UmeshSamartapu <umeshsamartapu@gmail.com>
2025-02-12 22:04:48 +0530

    simple form

---
+++ Nodejs-concepts-04/4 simpleForm.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<Head><title>THIS IS MY WEB PAGE</title></Head>');
+
+    if(req.url === '/'){
+        res.write('<h1>HOME PAGE</h1>');
+        return res.end();
+    }
+    else if(req.url === '/form')
+        {
+            res.write('<h1>HOME PAGE</h1>');
+            res.write('<form action="submit_form.php" method="post">  ');
+            res.write(' <label for="name">Name:</label> ');
+            res.write(' <input type="text" id="name" name="name" required> ');
+            res.write(' <br><br> ');
+
+            res.write(' <label>Gender:</label> ');
+            res.write(' <input type="radio" id="male" name="gender" value="male" required> ');
+            res.write(' <label for="male">Male</label>');
+            res.write(' <input type="radio" id="female" name="gender" value="female" required>');
+            res.write(' <label for="female">Female</label> ');
+            res.write(' <br><br> ');
+
+            res.write('  <button type="submit">Submit</button>');
+
+            return res.end();
+        }
+        
+    res.write('<footer><p1>Thank You</p1></footer>');
+    res.write('</html>');
+    res.end();
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`server running at http://localhost:3000 on port ${PORT}`);
+});
+
+
+
+// <form action="submit_form.php" method="post">
+// <label for="name">Name:</label>
+// <input type="text" id="name" name="name" required>
+// <br><br>
+
+// <label>Gender:</label>
+// <input type="radio" id="male" name="gender" value="male" required>
+// <label for="male">Male</label>
+// <input type="radio" id="female" name="gender" value="female" required>
+// <label for="female">Female</label>
+// <br><br>
+
+// <button type="submit">Submit</button>
